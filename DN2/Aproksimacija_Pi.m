10c2f66cd08413f744bdf405af22f225d510306b
LeonCepin <148787495+LeonCepin@users.noreply.github.com>
2023-11-13 22:00:15 +0100

    Add files via upload

---
+++ DN2/Aproksimacija_Pi.m
@@ -0,0 +1,19 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*monteCarloPiApproximationWithPlot[n_]:=Module[{pointsInside,ratio,insideColor,outsideColor},points=RandomReal[{-1,1},{n,2}];*)
+(*pointsInside=Select[points,Norm[#]<=1&];*)
+(*ratio=Length[pointsInside]/n;*)
+(*insideColor=RGBColor[0,0,1];*)
+(*outsideColor=RGBColor[1,0,0];*)
+(*insidePoints=Graphics[{insideColor,Point[pointsInside]}];*)
+(*outsidePoints=Graphics[{outsideColor,Point[Complement[points,pointsInside]]}];*)
+(*Print[4 ratio];*)
+(*Show[{insidePoints,outsidePoints},Epilog->Text["Aproksimirano \[SHacek]tevilo \[Pi]: "<>ToString[4 ratio],{0,-1.2},{0,1}],AspectRatio->1,ImageSize->400]]*)
+(**)
+(*n=10000.;*)
+(*monteCarloPiApproximationWithPlot[n]*)
+(**)
+
+
+
